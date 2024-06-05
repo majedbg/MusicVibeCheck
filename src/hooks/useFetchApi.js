dc6921dc86fe311ca967339cf0987c487e7246d6
Jade BuGhanem <majed.bg@gmail.com>
2024-06-05 17:59:43 -0400

    useFetchApi hook

--- src/hooks/useFetchApi.js
+++ src/hooks/useFetchApi.js
@@ -4,7 +4,7 @@ import { ApiContext } from './ApiContext';
 const useFetchApi = (url , userSecret, userId ) => {
   const { setApiResponse } = useContext(ApiContext);
   const fetchApi = async () => {
-    const fetchURL = 'https://99e6-2600-4041-57b7-ff00-945b-348a-b93c-645f.ngrok-free.app/tracks';
+    const fetchURL = 'https://f137-108-41-92-245.ngrok-free.app/tracks';
     const reqBody = ({
       client_id: userId,
       client_secret: userSecret
